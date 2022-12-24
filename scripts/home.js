eed640553c5e7b119ae6d0b36c3aa926c0901e79
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2022-12-24 16:03:21 -0600

    added media query ternary for responsive design

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,12 @@
-let home = `
-    <h1>Quinton Ulysses Pedrick</h1>
-`
+//console.log(window.innerWidth)
+
+let home = window.innerWidth > 600 ? 
+    `
+        <h1 class="name">Quinton Ulysses Pedrick</h1>
+    ` 
+    : 
+    `
+        <h1 class="first name">Quinton</h1>
+        <h1 class="middle name">Ulysses</h1>
+        <h1 class="last name">Pedrick</h1>
+    `
